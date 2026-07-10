f7f72cbb4bca53e0e6249085d3d928329321d9fd
Cyoku-ell <kokuofficial1@gmail.com>
2026-07-10 16:29:52 +0800

    last sebelum pulang

--- database/migrations/2014_10_11_000004_create_certificates_table.php
+++ database/migrations/2014_10_11_000004_create_certificates_table.php
@@ -12,26 +12,51 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('certificates', function (Blueprint $table) {
+
             $table->id();
             $table->foreignId('user_id')
-
+                ->nullable()
                 ->constrained()
-                ->cascadeOnUpdate()
-                ->cascadeOnDelete();
+                ->nullOnDelete();
+
+            $table->string('perner')->index();
 
             $table->string('title');
-            $table->date('start_date')->nullable();
-            $table->date('end_date')->nullable();
-            $table->date('issue_date');
-            $table->string('certificate_number');
+
+            $table->string('certificate_number')->unique();
+
             $table->string('registration_number')->nullable();
+
             $table->string('institution');
+
             $table->string('accreditor');
+
+            $table->date('issue_date');
+
+            $table->date('start_date')->nullable();
+
+            $table->date('end_date')->nullable();
+
             $table->string('pdf')->nullable();
+
+
+            // hasil matching owner
+            $table->boolean('is_matched')
+                ->default(false);
+
+    
             $table->text('remarks')->nullable();
+
+            // admin yang upload
+            $table->foreignId('created_by')
+                ->nullable()
+                ->constrained('users')
+                ->nullOnDelete();
+
             $table->timestamps();
         });
     }
+
     /**
      * Reverse the migrations.
      */
